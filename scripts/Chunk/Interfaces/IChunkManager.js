660bc1a6a2a16dbc18a78c3be626902b97128540
grimza_src <lofiboyofficialyt@gmail.com>
2026-08-14 06:40:50 -0500

    Create IChunkManager.js

---
+++ scripts/Chunk/Interfaces/IChunkManager.js
@@ -0,0 +1,15 @@
+class IChunkManager {
+    load(position) {
+        throw new Error("Method load not implemented");
+    }
+    unload(position) {
+        throw new Error("Method unload not implemented");
+    }
+    unloadAll() {
+        throw new Error("Method unloadAll not implemented");
+    }
+    list() {
+        throw new Error("Method list not implemented");
+    }
+}
+export { IChunkManager };
